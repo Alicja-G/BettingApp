39189e22ee2f22f178c41d26707b27fb6357aa52
Alicja Gadecka <gadecka.alicja@gmail.com>
2021-02-04 20:13:44 +0100

    basic routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent}, /** when someone browses to baseAddress/ homeComponent is loaded  */
+  { path: 'members', component: MemberListComponent},
+  { path: 'members/:id', component: MemberDetailComponent},
+  { path: 'lists', component: ListsComponent},
+  { path: 'messages', component: MessagesComponent},
+  { path: '**', component: HomeComponent, pathMatch: 'full'}, /** if users type rubbish, go to home component, pathMatch to prevent loop */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
